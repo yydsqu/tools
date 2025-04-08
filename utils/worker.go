92e59aca83e0a1ca293bc8211efdbe7b9e17e384
Fyd <21723614@qq.com>
2025-04-08 21:21:19 +0800

    init

--- utils/worker.go
+++ utils/worker.go
@@ -2,133 +2,85 @@ package utils
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"github.com/yydsqu/tools/log"
 	"runtime"
-	"sync"
+)
+
+var (
+	ErrorRestart = errors.New("worker restart")
 )
 
 type Handle func(ctx context.Context)
 
 type SingleWorker struct {
-	handle  Handle
-	wg      *sync.WaitGroup
-	ctx     context.Context
-	cancel  context.CancelCauseFunc
-	mu      sync.Mutex
-	err     error
-	running bool
-}
-
-func (sw *SingleWorker) SetHandle(handle Handle) {
-	sw.mu.Lock()
-	defer sw.mu.Unlock()
-	sw.handle = handle
-}
-
-func (sw *SingleWorker) StartHandle(handle Handle) {
-	sw.SetHandle(handle)
-	sw.Start()
-}
-
-func (sw *SingleWorker) RestartHandle(handle Handle) {
-	sw.Stop()
-	sw.StartHandle(handle)
+	handle Handle
+	ctx    context.Context
+	cancel context.CancelCauseFunc
 }
 
 func (sw *SingleWorker) Start() {
-	sw.mu.Lock()
-	defer sw.mu.Unlock()
-
 	sw.start()
 }
 
+func (sw *SingleWorker) Stop(err error) {
+	sw.cancel(err)
+}
+
 func (sw *SingleWorker) Restart() {
-	sw.Stop()
+	sw.cancel(ErrorRestart)
+	<-sw.ctx.Done()
 	sw.Start()
 }
 
-func (sw *SingleWorker) Stop() {
-	sw.mu.Lock()
-	defer sw.mu.Unlock()
-	if sw.running {
-		sw.cancel(context.Canceled)
-		sw.running = false
-	}
-}
-
-func (sw *SingleWorker) Error(err error) {
-	sw.mu.Lock()
-	defer sw.mu.Unlock()
-	if sw.running {
-		sw.cancel(err)
-		sw.running = false
-	}
-}
-
 func (sw *SingleWorker) Status() bool {
-	sw.mu.Lock()
-	defer sw.mu.Unlock()
-	return sw.running
+	select {
+	case <-sw.ctx.Done():
+		return false
+	default:
+		return true
+	}
 }
 
-func (sw *SingleWorker) Wait() error {
-	sw.wg.Wait()
-	return sw.err
+func (sw *SingleWorker) Done() <-chan struct{} {
+	return sw.ctx.Done()
+}
+
+func (sw *SingleWorker) Err() error {
+	return context.Cause(sw.ctx)
 }
 
 func (sw *SingleWorker) start() {
-	if sw.running {
-		log.Trace("worker is already running")
-		return
-	}
 	if sw.handle == nil {
-		log.Trace("worker handle is nil")
 		return
 	}
-	// 等待其他进程退出
-	sw.wg.Wait()
-	sw.running = true
-	sw.wg.Add(1)
+	select {
+	case <-sw.ctx.Done():
+	default:
+		return
+	}
 	sw.ctx, sw.cancel = context.WithCancelCause(context.Background())
-	sw.err = nil
-
-	go func() {
+	go func(ctx context.Context, cancel context.CancelCauseFunc) {
 		defer func() {
-			if err := Recover(); err != nil {
-				fmt.Println(err)
-				log.Error("worker recover panic", "err", err)
-				sw.err = err
+			var err error
+			if r := recover(); r != nil {
+				buf := make([]byte, 1024)
+				n := runtime.Stack(buf, false)
+				err = fmt.Errorf("panic: %v\nstack trace:\n%s", r, buf[:n])
 			}
-			sw.wg.Done()
-			sw.Stop()
+			cancel(err)
 		}()
-		sw.handle(sw.ctx)
-	}()
+		sw.handle(ctx)
+	}(sw.ctx, sw.cancel)
 }
 
 func NewSingleWorker(handle Handle) *SingleWorker {
 	ctx, cancel := context.WithCancelCause(context.Background())
+	defer cancel(nil)
 	sw := &SingleWorker{
 		handle: handle,
 		ctx:    ctx,
 		cancel: cancel,
-		wg:     &sync.WaitGroup{},
 	}
 	return sw
-}
-
-func Recover() error {
-	if r := recover(); r != nil {
-		buf := make([]byte, 1024)
-		n := runtime.Stack(buf, false)
-		return fmt.Errorf("panic: %v\nstack trace:\n%s", r, buf[:n])
-	}
-	return nil
-}
-
-func PrintStack() {
-	buf := make([]byte, 1024)
-	n := runtime.Stack(buf, false)
-	fmt.Printf("stack trace:\n%s\n", buf[:n])
 }
--- utils/worker_test.go
+++ utils/worker_test.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"context"
 	"fmt"
-	"sync"
 	"testing"
 	"time"
 )
@@ -24,56 +23,10 @@ func exampleHandle(ctx context.Context) {
 	}
 }
 
-type CondWorker struct {
-	*sync.Mutex
-	*sync.Cond
-	handles []Handle
-	ctx     context.Context
-	cancel  context.CancelCauseFunc
-	ready   bool
-}
-
-func (worker *CondWorker) Start() {
-	worker.Lock()
-	defer worker.Unlock()
-	worker.ready = true
-	worker.Broadcast()
-}
-
-func (worker *CondWorker) run() {
-	for i, handle := range worker.handles {
-		go func(handle Handle, i int) {
-			worker.Lock()
-			defer worker.Unlock()
-			for !worker.ready {
-				worker.Wait()
-			}
-			handle(worker.ctx)
-		}(handle, i)
-	}
-}
-
-func (worker *CondWorker) Stop() {
-
-}
-
-func NewCondWorker(handles ...Handle) *CondWorker {
-	ctx, cancel := context.WithCancelCause(context.Background())
-	mutex := &sync.Mutex{}
-	cond := &CondWorker{
-		ctx:     ctx,
-		cancel:  cancel,
-		handles: handles,
-		Mutex:   mutex,
-		Cond:    sync.NewCond(mutex),
-	}
-	cond.run()
-	return cond
-}
-
 func TestName(t *testing.T) {
-	worker := NewCondWorker(exampleHandle, exampleHandle, exampleHandle)
-	time.Sleep(time.Second)
+	worker := NewSingleWorker(exampleHandle)
 	worker.Start()
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second)
+	worker.Restart()
+	time.Sleep(time.Second)
 }
