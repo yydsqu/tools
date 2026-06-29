da99571cb2b88975f2284f218f6dc8ade5c97b85
shuziliu2022 <shuziliu2022@gmail.com>
2026-06-29 14:26:33 +0800

    Switched to `math/rand/v2` and updated RNG to use `rand.NewPCG` for improved randomness.

--- balancer/random.go
+++ balancer/random.go
@@ -2,8 +2,7 @@ package balancer
 
 import (
 	"errors"
-	"math/rand"
-	"time"
+	"math/rand/v2"
 )
 
 type Random[T any] struct {
@@ -16,17 +15,16 @@ func (p *Random[T]) Items() []T {
 }
 
 func (p *Random[T]) Next() T {
-	return p.items[p.rnd.Intn(len(p.items))]
+	return p.items[p.rnd.IntN(len(p.items))]
 }
 
 func NewRandom[T any](items ...T) (*Random[T], error) {
 	if len(items) == 0 {
 		return nil, errors.New("empty items")
 	}
-
 	return &Random[T]{
 		items: items,
-		rnd:   rand.New(rand.NewSource(time.Now().UnixNano())),
+		rnd:   rand.New(rand.NewPCG(rand.Uint64(), rand.Uint64())),
 	}, nil
 }
 
@@ -36,6 +34,6 @@ func MustRandom[T any](items ...T) *Random[T] {
 	}
 	return &Random[T]{
 		items: items,
-		rnd:   rand.New(rand.NewSource(time.Now().UnixNano())),
+		rnd:   rand.New(rand.NewPCG(rand.Uint64(), rand.Uint64())),
 	}
 }
